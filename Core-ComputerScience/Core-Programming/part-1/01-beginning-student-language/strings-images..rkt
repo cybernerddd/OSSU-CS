77d30debb0b4cc7b7cc053ec0c0235ef6ca3fbe4
cybernerddd <cybernerddd@gmail.com>
2026-09-17 21:59:39 +0000

    Add string manipulation and graphics practices

--- Core-ComputerScience/Core-Programming/part-1/01-beginning-student-language/strings-images..rkt
+++ Core-ComputerScience/Core-Programming/part-1/01-beginning-student-language/strings-images..rkt
@@ -25,3 +25,33 @@
        
 (overlay (text "STOP" 48 "white")
          (regular-polygon 60 8 "solid" "red"))
+
+
+(substring "Crocodile" 0 4)
+(substring "Crocodile" 5)
+(string-length "Cybersecurity")
+(string-length "Cybernerddd")
+(string-length "Ethical hacker")
+(string-append "I" " " "am" " " "learning" " " "Racket.")
+
+(above (circle 10 "solid" "red")
+       (circle 10 "solid" "yellow")
+       (circle 10 "solid" "green"))
+
+
+(overlay (text "WARNING" 30 "white")
+         (regular-polygon 66 8 "solid" "red"))
+
+
+(overlay 
+         (beside (text "HACK" 30 "black")
+                 (circle 10 "solid" "blue"))
+         (rectangle 200 60 "solid" "green"))
+
+(overlay (rectangle 150 90 "outline" "black")
+ (above (text "CYBER" 20 "black")
+        (beside (circle 10 "solid" "blue")
+        (circle 10 "solid" "green")
+        (circle 10 "solid" "red"))
+        (text "NERDDD" 20 "black")))
+        
