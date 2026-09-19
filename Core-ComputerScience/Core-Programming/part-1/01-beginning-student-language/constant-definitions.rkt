f9d4a2b213d797520febecb12f9af902abe868f4
cybernerddd <cybernerddd@gmail.com>
2026-09-19 14:27:50 +0000

    Add constant definitions for image dimensions and rotation

---
+++ Core-ComputerScience/Core-Programming/part-1/01-beginning-student-language/constant-definitions.rkt
@@ -0,0 +1,16 @@
+
+(require 2htdp/image)
+
+
+(define WIDTH 400)
+(define HEIGHT 600)
+
+(* WIDTH HEIGHT)
+
+(define CAT <img_pasted>)
+
+(rotate -10 CAT)
+(rotate 90 CAT)
+
+(define RCAT (rotate 10 CAT))
+(define LCAT (rotate 90 CAT))
