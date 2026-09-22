c8f7d5ef903c7e397268119e0df0311555139a86
cybernerddd <cybernerddd@gmail.com>
2026-09-22 02:26:33 +0000

    Add various utility functions for graphics and badges

--- Core-ComputerScience/Core-Programming/part-1/01-beginning-student-language/functions.rkt
+++ Core-ComputerScience/Core-Programming/part-1/01-beginning-student-language/functions.rkt
@@ -20,3 +20,43 @@
   (* h w))
 
 (area 10 2)
+
+; rectangle area
+(define (rectangle-area h w)
+  (* h w))
+
+(rectangle-area 10 2)
+
+;greeting
+(define (greet name)
+  (string-append "Hello " name "!"))
+(greet "nana")
+
+; logo
+(define (logo sign)
+  (text sign 30 "red"))
+
+(logo "cybernerddd")
+
+;badge maker
+(define (make-badge f-name alias)
+  (overlay
+   (above (text f-name 15 "blue")
+          (text alias 10 "blue"))
+   (rectangle 120 80 "solid" "grey")))
+
+(make-badge "Emmanuel" "cybernerddd")
+
+;celsius-to-fahrenheit
+(define (celsius-to-fahrenheit celsius)
+  (+ (* celsius 1.8) 32))
+
+(celsius-to-fahrenheit 25)
+
+;testing function in fnction
+(define (cyber-badge name)
+  (overlay (above (logo name)
+                  (text "cybernerddd" 12 "red"))
+           (square 100 "outline" "blue")))
+
+(cyber-badge "MAX")
